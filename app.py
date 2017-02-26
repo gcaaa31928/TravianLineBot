a1e228d42447245e1f8367f6f11563bbb357c6bc
gcaaa31928 <gcaaa31928@gmail.com>
2017-02-26 21:59:08 +0800

    test logger

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def hello():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
