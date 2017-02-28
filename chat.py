af45a529d31dc79d6a20afced7d29344c2498bdb
gcaaa31928 <gcaaa31928@gmail.com>
2017-02-28 22:41:19 +0800

    remove chinese word

--- chat.py
+++ chat.py
@@ -4,8 +4,5 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 chatbot = ChatBot("myBot")
 chatbot.set_trainer(ChatterBotCorpusTrainer)
 
-# 使用英文语料库训练它
 chatbot.train("chatterbot.corpus.chinese")
 
-# 开始对话
-print(chatbot.get_response("你住哪裡"))
